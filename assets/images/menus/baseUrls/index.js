e3424e46f5db5d410806f3e8a482dd89852ba87f
lld <hilarywwtha@gmail.com>
2024-01-16 10:29:55 +0800

    feat: 🎸 新增 module.exports

--- assets/images/menus/baseUrls/index.js
+++ assets/images/menus/baseUrls/index.js
@@ -2,8 +2,12 @@ import meatDishes1 from './meatDishes1';
 import meatDishes2 from './meatDishes2';
 import meatDishes3 from './meatDishes3';
 
-export default {
+const allUrls = {
     meatDishes1,
     meatDishes2,
     meatDishes3
 }
+
+export default allUrls
+
+module.exports = allUrls
