5f716cf210da3ab71be3f8a0a8f62bb41c56f331
lld <hilarywwtha@gmail.com>
2024-01-16 10:45:30 +0800

    feat: 🎸 新增 meatDishes4.js

--- assets/images/menus/baseUrls/index.js
+++ assets/images/menus/baseUrls/index.js
@@ -1,13 +1,21 @@
-import meatDishes1 from './meatDishes1';
-import meatDishes2 from './meatDishes2';
-import meatDishes3 from './meatDishes3';
+// import meatDishes1 from './meatDishes1';
+// import meatDishes2 from './meatDishes2';
+// import meatDishes3 from './meatDishes3';
+
+const { meatDishes1 } = require("./meatDishes1");
+const { meatDishes2 } = require("./meatDishes2");
+const { meatDishes3 } = require("./meatDishes3");
+const { meatDishes4 } = require("./meatDishes4");
 
 const allUrls = {
     meatDishes1,
     meatDishes2,
-    meatDishes3
+    meatDishes3,
+    meatDishes4
 }
 
 export default allUrls
 
-module.exports = allUrls
+module.exports = {
+    allUrls
+}
