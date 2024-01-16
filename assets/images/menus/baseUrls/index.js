b1a5db2662b78b884f8e7f739c8ad3e2ad48bbaf
lld <hilarywwtha@gmail.com>
2024-01-16 10:17:23 +0800

    feat: 🎸 新增背景图、base64资源

---
+++ assets/images/menus/baseUrls/index.js
@@ -0,0 +1,9 @@
+import meatDishes1 from './meatDishes1';
+import meatDishes2 from './meatDishes2';
+import meatDishes3 from './meatDishes3';
+
+export default {
+    meatDishes1,
+    meatDishes2,
+    meatDishes3
+}
